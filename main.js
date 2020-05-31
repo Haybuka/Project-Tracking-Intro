6d5c270f06320ee6cfe5880b075c5a249042cb00
Haybuka <paschal.nduleo@gmail.com>
2020-05-31 18:37:46 +0100

    Created Js togglers Toggled between open and close for the nav also used setAttribute with conditionals,to toggle the harmburger,i was actually sceptical it would work,but it did .. using e.srcElement.src,via Js DOM, i was able to achieve the toggle between the 3 bars and the X

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const harmburger = document.querySelector('#container nav .dropDown')
-const harmburgerX = document.querySelector(' #container nav .canceldrop')
+const harmburger = document.querySelector('.dropDown')
+const dropimg = document.querySelector('.dropDown img')
 const nav = document.querySelector('#container nav ul')
 
-harmburger.addEventListener('click', function () {
-    nav.classList.toggle('open')
-    console.log(this, harmburgerX, nav)
+harmburger.addEventListener('click', (e) => {
+    //toggle class list for harmburger
+    //using the evemt element,toggle src for closed harm and open
+    nav.classList.toggle('open');
+    if ((e.srcElement.src) == 'file:///C:/Users/precious/Desktop/HtmlCss%20Project/project-tracking-intro-component-master/images/icon-hamburger.svg') {
+        //using setAttribute and conditional,Check what Element exist,then set
+        dropimg.setAttribute('src', 'images/icon-close.svg');
+    } else {
+        dropimg.setAttribute('src', 'images/icon-hamburger.svg');
+    }
 })
